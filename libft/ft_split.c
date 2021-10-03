e1b46d0607a8051a585b0f3122b664c391eb8154
Diogo Pereira <diogo.pereira.ee@gmail.com>
2021-10-03 20:08:11 +0100

    update ft_split.c

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,23 +6,21 @@ char	*ft_substr(char const *s, unsigned int start, size_t len);
 
 size_t	get_size(char const *s, char c)
 {
-	size_t	i;
 	size_t	size;
 	int		flag;
 
-	i = 0;
 	flag = 0;
 	size = 0;
-	while (s[i] != '\0')
+	while (*s != '\0')
 	{
-		if (s[i] != c && !flag)
+		if (*s != c && !flag)
 		{
 			size++;
 			flag = 1;
 		}
-		else if (s[i] == c)
+		else if (*s == c)
 			flag = 0;
-		i++;
+		s++;
 	}
 	return (size + 1);
 }
@@ -30,26 +28,22 @@ size_t	get_size(char const *s, char c)
 char	*get_str(char const *s, char c, size_t offset)
 {
 	size_t	init;
-	size_t	end;
 
 	init = offset;
 	while (s[offset] != c && s[offset] != '\0')
 		offset++;
-	end = offset;
-	return (ft_substr(s, init, (end - init)));
+	return (ft_substr(s, init, (offset - init)));
 }
 
 char	**ft_split(char const *s, char c)
 {
-	size_t	arr_size;
 	char	**arr;
 	size_t	i;
 	size_t	j;
 
 	if (!s)
 		return (0);
-	arr_size = get_size(s, c);
-	arr = malloc(arr_size * sizeof(char *));
+	arr = malloc(get_size(s, c) * sizeof(char *));
 	if (!arr)
 		return (0);
 	i = 0;
