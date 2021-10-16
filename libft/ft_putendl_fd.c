38244b964f021c3f2232241b278ec53ab41f081b
Diogo Pereira <diogo.pereira.ee@gmail.com>
2021-10-16 17:46:06 +0100

    added ft_putendl_fd.c

---
+++ libft/ft_putendl_fd.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+void	ft_putendl_fd(char *s, int fd)
+{
+	if (s && fd > 0)
+	{
+		while (*s != '\0')
+		{
+			write(fd, &(*s), 1);
+			s++;
+		}
+		write(fd, "\n", 1);
+	}		
+}
