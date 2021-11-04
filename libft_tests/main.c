c05382d9d6e76b2b797afa99a241e7cb78857602
Diogo Pereira <diogo.pereira.ee@gmail.com>
2021-11-04 19:31:21 +0000

    added more cases of test to main.c

--- libft_tests/main.c
+++ libft_tests/main.c
@@ -1,7 +1,7 @@
 #include "test_lib.h"
-#include "../libft.h"
+#include "../libft/libft.h"
 
-//gcc main.c test_lib.c test_lib.h ../libft.a
+//gcc main.c test_lib.c test_lib.h ../libft/libft.a
 
 int main(void)
 {
@@ -15,5 +15,9 @@ int main(void)
   test("ft_toupper", "arz{`3AZ", &toupper, &ft_toupper, &int_verify);
   test("ft_tolower", "ARZ@[3az", &tolower, &ft_tolower, &int_verify);
 
-    return (0);
+  printf("\n\nft_substr: |%s|", ft_substr("test", 2, 60));
+  printf("\nft_strjoin: |%s|\n\n", ft_strjoin("test", "123"));
+  printf("\nft_strtrim: |%s|", ft_strtrim("        a   ", " "));
+  
+  return (0);
 }
