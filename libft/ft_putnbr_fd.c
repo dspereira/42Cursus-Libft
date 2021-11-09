74b39ec7cc1fe718afcad61d753e26c0cd3ee440
Diogo Silverio pereira <dsilveri@c1r12s1.42lisboa.com>
2021-11-09 12:14:45 +0000

    fix ft_putnbr_fd.c

--- libft/ft_putnbr_fd.c
+++ libft/ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: dsilveri <dsilveri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 11:33:01 by dsilveri          #+#    #+#             */
-/*   Updated: 2021/11/03 11:33:02 by dsilveri         ###   ########.fr       */
+/*   Updated: 2021/11/09 12:01:43 by dsilveri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,13 @@ void	ft_putnbr_fd(int n, int fd)
 
 	if (fd < 0)
 		return ;
-	nb = n;
+	nb = (long int)n;
 	if (n < 0)
 	{
 		write(fd, "-", 1);
 		nb *= -1;
 	}
-	if (nb > 10)
+	if (nb >= 10)
 		ft_putnbr_fd((nb / 10), fd);
 	c = nb - ((nb / 10) * 10) + 48;
 	write(fd, &c, 1);
