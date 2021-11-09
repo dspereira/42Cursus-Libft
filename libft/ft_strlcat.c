8b6173e684d2767dd1da27a02d8dbf65f433acef
Diogo Silverio pereira <dsilveri@c1r12s1.42lisboa.com>
2021-11-09 10:46:15 +0000

    fix ft_strlcat.c

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: dsilveri <dsilveri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 11:33:38 by dsilveri          #+#    #+#             */
-/*   Updated: 2021/11/03 15:00:13 by dsilveri         ###   ########.fr       */
+/*   Updated: 2021/11/09 10:45:12 by dsilveri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,18 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 	size_t	src_size;
 	size_t	i;
 
-	i = 0;
 	if (!dst && !src)
 		return (0);
 	dst_size = ft_strlen(dst);
 	src_size = ft_strlen(src);
-	if (size < dst_size)
+	if (size <= dst_size)
 		return (src_size + size);
-	while (src[i] != '\0' && i < size - 1 - dst_size)
+	i = dst_size;
+	while (src[i - dst_size] != '\0' && i < size - 1)
 	{
-		dst[dst_size + i] = src[i];
+		dst[i] = src[i - dst_size];
 		i++;
 	}
-	dst[dst_size + i] = '\0';
+	dst[i] = '\0';
 	return (dst_size + src_size);
 }
